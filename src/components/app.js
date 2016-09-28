92d1b9d9c2b477812161109483e7c61458754002
Andrew D.Laptev <a.d.laptev@gmail.com>
2016-09-28 22:57:22 +0300

    Way to redux

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Footer from './pages/footer';
 
 class App extends Component {
 	render() {
@@ -10,7 +11,7 @@ class App extends Component {
 					{this.props.children}
 				</div>
 
-				<p>Footer here</p>
+				<Footer />
 			</div>
 		);
 	}
